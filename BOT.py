68fbe43dcd3a3f379de23d6ca2f93748f8ca4100
Arun Kumar <rarunkumar15112k@gmail.com>
2021-01-20 00:13:30 +0530

    Bot Check

--- BOT.py
+++ BOT.py
@@ -1,14 +1,12 @@
 import pandas as pd
-pd.options.mode.chained_assignment = None
-df = pd.DataFrame(index=[0,1,2])
-newcsvname="bot1.csv"
-df.to_csv(newcsvname)
 import botometer
 import tweepy
-consumer_key="5tlYTpVNtcxNR1tmPTcgMBC0j"
-consumer_secret="wVEX9My3v6340sHINjsJ2LVj6lmxxWPuo6KgALIMkUhlTu5lH4"
-access_token="1002549079774093313-fCET9YfrjgVJLe1gZy1n8O4mnArToO"
-access_token_secret="gNNGpKh7GBsQZYaguflQX5QXFSCER4mKyp5IVdOMcVKvE"
+pd.options.mode.chained_assignment = None
+
+consumer_key="Insert_consumer_key_here"
+consumer_secret="Insert_consumer_secret_here"
+access_token="Insert_access_token_here"
+access_token_secret="Insert_access_token_secret_here"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -16,13 +14,15 @@ if (api):
     print("Login Success")
 else:
     print("Failed")
-rapidapi_key = "e8ac519084mshbec641a05aca5f1p1f553bjsn92af315e6906" # now it's called rapidapi key
+    
+rapidapi_key = "Insert_rapidapi_key_here" 
 twitter_app_auth = {
     'consumer_key': consumer_key,
     'consumer_secret': consumer_secret,
     'access_token': access_token,
     'access_token_secret': access_token_secret,
   }
+
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
@@ -31,7 +31,9 @@ bom = botometer.Botometer(wait_on_ratelimit=True,
 user_name_list=['@narendramodi','@MetaphorMagnet','@DeepDrumpf']
 #Get the user name list and save it to user_name_list take max of 1999 and also add '@' symbol before using user = '@'+ user
 
-
+newcsvname="bot1.csv"
+df = pd.DataFrame(index=[0,1,2])
+df.to_csv(newcsvname)
 data=pd.read_csv(newcsvname)
 data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 data['user']=" "
