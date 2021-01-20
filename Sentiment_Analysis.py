6728508d83984b36997942ee1d908c38aadcd628
Arun Kumar <rarunkumar15112k@gmail.com>
2021-01-20 12:28:47 +0530

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -1,20 +1,25 @@
+# To perform Sentiment analysis of the tweets using VADER sentiment analysis
+
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
+import re
+
 pd.options.mode.chained_assignment = None 
 analyser = SentimentIntensityAnalyzer()
 sentiment=''
-import re
-csvname="twittr1229.csv"
+csvname="sentiment.csv"
 data=pd.read_csv(csvname)
 l=[]
 data.drop(data.columns[data.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 rows=len(data)
 data['sentiment']=" "
+
 for i in range(0,rows):
     s=data['tweetText'][i]
     l.append(s)
     sentiment_data=analyser.polarity_scores(s)
     score=sentiment_data['compound']
+    # Polarity score ranges from -1 (most extreme negative) to +1 (most extreme positive)
     if (score >= 0.05):
         sentiment=' positive '
     elif (score<0.05 and score>-0.05):
@@ -22,5 +27,6 @@ for i in range(0,rows):
     else:
         sentiment='negative'
     data['sentiment'][i]=sentiment
+    
 data.to_csv(csvname,index=False)
 
