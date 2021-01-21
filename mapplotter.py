18aecbf2c88f59f94ce31dfcacb46b95c8b7e406
Arun Kumar <rarunkumar15112k@gmail.com>
2021-01-21 23:29:29 +0530

    mapplotter

--- mapplotter.py
+++ mapplotter.py
@@ -2,41 +2,29 @@ import pandas
 from geopy.geocoders import Nominatim
 import gmplot
 
-
-colnames=['latitude','longitude']
-data=pandas.read_csv('twittr117.csv',names=colnames)
-latitude=data.latitude.tolist()
-longitude=data.longitude.tolist()
-for i in range(0,latitude.count('99999.99')):
-    latitude.remove('99999.99')
-for i in range(0,longitude.count('9999.99')):
-    longitude.remove('9999.99')
-latitude=latitude[1:]
-longitude=longitude[1:]
-latitude=list(map(float, latitude))
-longitude=list(map(float, longitude))
-
-data=pandas.read_csv('twittr118.csv',names=colnames)
-latitude1=data.latitude.tolist()
-longitude1=data.longitude.tolist()
-for i in range(0,latitude1.count('99999.99')):
-    latitude1.remove('99999.99')
-for i in range(0,longitude1.count('9999.99')):
-    longitude1.remove('9999.99')
-latitude1=latitude1[1:]
-longitude1=longitude1[1:]
-latitude1=list(map(float, latitude1))
-longitude1=list(map(float, longitude1))
-latitude.extend(latitude1)
-longitude.extend(longitude1)
-print(latitude)
-print(longitude)
+latitude_final=[]
+longitude_final=[]
+for name in range(1,6):
+    data=pandas.read_csv('repub'+str(name)+'.csv')
+    latitude=data.latitude.tolist()
+    longitude=data.longitude.tolist()
+    # Non Geo-tagged tweets were given a value of (99999.99,99999.99). We are removing them.
+    for i in range(0,latitude.count(99999.99)):
+        latitude.remove(99999.99)
+    for i in range(0,longitude.count(99999.99)):
+        longitude.remove(99999.99)
+    latitude=list(map(float, latitude))
+    longitude=list(map(float, longitude))
+    latitude_final.extend(latitude)
+    longitude_final.extend(longitude)
+    print(len(latitude_final))
+    print(len(longitude_final))
 gmap = gmplot.GoogleMapPlotter(30, 0, 3)
 
 
-# Overlay our datapoints onto the map
-gmap.heatmap(latitude, longitude)
+# Overlay the datapoints into the map
+gmap.heatmap(latitude_final, longitude_final)
 
 # Generate the heatmap into an HTML file
-gmap.draw("fina234.html")
+gmap.draw("repub_map.html")
 
