59e5ab74ac0ba3ad57b3ed5f3666fe518ce2c23f
Arun Kumar <rarunkumar15112k@gmail.com>
2021-01-21 22:15:40 +0530

    Filter_by_user

--- Filter_by_user.py
+++ Filter_by_user.py
@@ -23,7 +23,7 @@ else:
 
 class TweetStreamListener(tweepy.StreamListener):
     def on_data(self,data):
-        
+        #dumping json data
         tweet=json.loads(data)
         try:
                 def from_creator(data):
@@ -31,40 +31,37 @@ class TweetStreamListener(tweepy.StreamListener):
                         return False
                     else:
                         return True
-                if from_creator(data):                
+                if from_creator(data):     
+                    # To filter out retweets and to access tweets greater than 140 characters in length.
                     if  not tweet["text"].startswith('RT'):
                         if tweet.get("extended_tweet"):
                             text = str(tweet['extended_tweet']["full_text"])
                         else:
                             text= str(tweet["text"])
                         print(text)
-            
                 return True
                 
         except Exception as e:
-            print()
-            print("")
+            print(e)
             return True
         
 if __name__ == '__main__':
     print("start \n")
-    
     # Run the stream!
     l = TweetStreamListener()
     stream = tweepy.Stream(auth, l,tweet_mode='extended')
-    stream.filter(follow=['insert_twitter_id_here']) 
     # Filtering
     while not stream.running:
         try:
             logging.info("Started listening to twitter stream...")
-            stream.filter(follow=['1204812731620057088'])            
+            stream.filter(follow=['insert_twitter_id_here'])             
         except(ProtocolError, AttributeError):
             continue
         except(Timeout, SSLError, ReadTimeoutError, ConnectionError) as e:
-            logging.warning("Network error occurred. Keep calm and carry on.", str(e))
+            logging.warning("Network error occurred.", str(e))
         except Exception as e:
-            logging.error("Unexpected error!", e)
+            logging.error("Unexpected error.", e)
         finally:
-            logging.info("Stream has crashed. System will restart twitter stream!")
-    logging.critical("Somehow zombie has escaped...!")
+            logging.info("Stream has crashed.")
+    logging.critical("Somehow zombie has escaped.")
 
